faa5f9867605d338537e35e069156c577ac1e797
Alexey <kar-alekej@yandex.ru>
2021-08-18 01:35:29 +0300

    Work with logic

--- AppAK/ViewController.swift
+++ AppAK/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var A1985KLabel: UILabel!
+    @IBOutlet weak var StartButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        A1985KLabel.isHidden = true
+        A1985KLabel.textColor = .systemRed
+        StartButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func StartButtonPressed() {
+        A1985KLabel.isHidden.toggle()
+        
+        if A1985KLabel.isHidden {
+            StartButton.setTitle("Show text", for: .normal)
+        } else {
+            StartButton.setTitle("Hide text", for: .normal)
+          }
+    }
 }
 
